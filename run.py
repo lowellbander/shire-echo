f23e57bcb60f511540f69ac35de8c908489ae335
Lowell Bander <lowellbander@gmail.com>
2015-02-21 21:16:05 -0800

    echo implemented

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+import json
+import requests
+import os
+
+#WEBHOOK_URL = 'https://hooks.slack.com/services/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'
 
 app = Flask(__name__)
 app.debug = True
@@ -10,10 +15,14 @@ def index():
 @app.route('/echo', methods=['GET', 'POST'])
 def echo():
     if request.method == 'POST':
-        print request.data
-        return request.data
+        text = request.form.get('text')
+        payload = {'text': text}
+        payload = json.dumps(payload)
+        r = requests.post(os.environ['WEBHOOK_URL'], data=payload)
+        return r.text
     else:
+        print 'received GET'
         return 'you did a GET'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
