999d28eeaf52fda5f146aed7e56522439acc5726
Lowell Bander <lowellbander@gmail.com>
2015-02-22 15:08:00 -0800

    added simple /propose

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ import json
 import requests
 import os
 
-#WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+general_webhook='https://hooks.slack.com/services/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'
+
+government_webhook = "https://hooks.slack.com/services/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"
 
 app = Flask(__name__)
 app.debug = True
@@ -18,7 +20,22 @@ def echo():
         text = request.form.get('text')
         payload = {'text': text}
         payload = json.dumps(payload)
-        r = requests.post(os.environ['WEBHOOK_URL'], data=payload)
+        r = requests.post(general_webhook, data=payload)
+        return r.text
+    else:
+        print 'received GET'
+        return 'you did a GET'
+
+@app.route('/propose', methods=['GET', 'POST'])
+def propose():
+    if request.method == 'POST':
+        proposition = request.form.get('text')
+        user_name = request.form.get('user_name')
+        payload = {'text': user_name + 
+                " proposes the following legislation: \""
+                + proposition + "\""}
+        payload = json.dumps(payload)
+        r = requests.post(government_webhook, data=payload)
         return r.text
     else:
         print 'received GET'
