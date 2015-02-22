185de4d545ba38b6ccf5676227c4af516d90e10e
Lowell Bander <lowellbander@gmail.com>
2015-02-21 20:33:52 -0800

    prints

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ def index():
 @app.route('/echo', methods=['GET', 'POST'])
 def echo():
     if request.method == 'POST':
+        print request.data
         return request.data
     else:
         return 'you did a GET'
